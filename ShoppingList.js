cf2f41f72ba25f6ee3f947a825f3d1b8c41efc7e
meunier <meunier@intuilab.com>
2020-06-08 09:28:56 -0500

    add copyright 2020 and update email wording

--- ShoppingList.js
+++ ShoppingList.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright � 2014 Intuilab
+ * Copyright © 2020 Intuilab
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
  * to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
